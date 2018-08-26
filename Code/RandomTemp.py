9ba51e4355f9e41603685d1552ba53b2251c4fbe
Rudra <37140269+rudrathegreat@users.noreply.github.com>
2018-08-26 21:56:42 +1000

    Add files via upload

--- Code/RandomTemp.py
+++ Code/RandomTemp.py
@@ -3,10 +3,13 @@ from random import randint
 from time import sleep
 
 while True:
-    temp = randint(15, 35)
-    path = 'D:\Rudra\Python\Code\Displayer\sampleText.txt'
+
+    temp = randint(150, 350)/10
+    cond = randint(1100, 1450)
+    pH = randint(300, 900)/100
+    path = 'files\sampleText.txt'
     file_handle = open(path, 'a')
     present = datetime.datetime.now()
-    file_handle.write(str(present) + ',' + str(temp) + "\n")
+    file_handle.write(str(present) + ',' + str(temp) + ',' + str(cond) + ',' + str(pH) + "\n")
     file_handle.close()
     sleep(20)
